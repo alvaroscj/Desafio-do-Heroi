922b95b7c569d79cfba6ea7293e1572e8085433a
Alvaro Santana <alvaroscj@gmail.com>
2025-08-25 13:01:11 +0000

    Desafio Github

---
+++ Desafio Ferramentas Github/repet_txt.py
@@ -0,0 +1,6 @@
+# Solicita uma string e um número inteiro como entrada do usuário
+
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((texto + "\n") * numero)  # Imprime a string repetida o número de vezes especificado, cada repetição em uma nova linha
